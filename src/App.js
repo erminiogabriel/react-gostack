57b4ba39234387c874f4cacd306014ac80023c3d
Gabriel Machado <gabriel@movitech.com.br>
2020-07-03 13:56:44 -0300

    Finalizado desenvolvimento dos conceitos basicos do react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
@@ -12,7 +11,14 @@ import Header from './components/Header';
  */
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(respose => {
+            console.log(respose);
+            setProjects(respose.data)
+        });
+    }, []);
     /**
      * useState retorna um array com 2 posicoes
      * 
@@ -21,20 +27,23 @@ function App() {
      */
 
 
-    function handleAddProject() {
+    async function handleAddProject() {
         //projects.push(`Novo Projeto ${Date.now()}`);
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
-        console.log(projects);
+        //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        const response = await api.post('projects', {
+                title: `mobile com react native ${Date.now()}`,
+                owner: "gabriel"
+            });
+        const project = response.data;
+        setProjects([...projects, project]);
     }
 
     return (
         <>
         <Header title="Projects"/>
 
-        <img width={400} src={backgroundImage} />
-
         <ul>
-            {projects.map(project => <li key={project}>{project}</li>)}
+            {projects.map(project => <li key={project.id}>{project.title}</li>)}
         </ul>
 
         <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
